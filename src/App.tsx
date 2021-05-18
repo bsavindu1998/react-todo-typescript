1f11d0caffb4164bef1edaf0497fe14d2803e7e8
Savindu Bandara <bsavindu1998@gmail.com>
2021-05-18 22:44:51 +0530

    added list items

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import { type } from "os";
+import React from "react";
+import { ToDoListItem } from "./TodoListItem";
 
-
-
+const todos: Array<Todo> = [
+  { text: "walk the dog", complete: true },
+  { text: "Write the app", complete: false },
+];
 function App() {
   return (
-    <div>Hello World!</div>
+    <React.Fragment>
+      <ToDoListItem todo={todos[0]} />
+      <ToDoListItem todo={todos[1]} />
+    </React.Fragment>
   );
 }
 
---
+++ src/TodoListItem.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./TodoListItem.css";
+
+interface TodoListItemProps {
+  todo: Todo;
+}
+
+export const ToDoListItem: React.FC<TodoListItemProps> = ({ todo }) => {
+  return (
+    <li>
+      <label className={todo.complete ? "complete" : "none"}>
+        <input type="checkbox" checked={todo.complete} />
+        {todo.text}
+      </label>
+    </li>
+  );
+};
