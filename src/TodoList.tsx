1167313dae5f38a2fc2dcfca0cd1828401586d26
Savindu Bandara <bsavindu1998@gmail.com>
2021-05-19 13:34:00 +0530

    code formatted

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import { ToDoListItem } from './TodoListItem';
+import React from "react";
+import { ToDoListItem } from "./TodoListItem";
 
-
-interface TodoListProps{
-    todos:Array<Todo>;
-    toggleTodo:ToggleTodo;
+interface TodoListProps {
+  todos: Array<Todo>;
+  toggleTodo: ToggleTodo;
 }
 
-export const ToDoList:React.FC<TodoListProps> = ({todos,toggleTodo}) =>{
-    return <ul>
-        {todos.map(todo=>{
-            return<ToDoListItem key={todo.text} todo = {todo} toggleTodo={toggleTodo}/>
-        })}
+export const ToDoList: React.FC<TodoListProps> = ({ todos, toggleTodo }) => {
+  return (
+    <ul>
+      {todos.map((todo) => {
+        return (
+          <ToDoListItem key={todo.text} todo={todo} toggleTodo={toggleTodo} />
+        );
+      })}
     </ul>
-}
+  );
+};
